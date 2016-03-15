5bb6823b4d344fe54de20d1f365c2f0a3a92ec27
Shenhuanet <shenhuanet@126.com>
2016-03-15 12:36:38 +0800

    1231231

--- app/src/main/java/com/shenhua/reading/MainActivity.java
+++ app/src/main/java/com/shenhua/reading/MainActivity.java
@@ -3,6 +3,8 @@ package com.shenhua.reading;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -11,21 +13,29 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        toolbar.setLogo(getResources().getDrawable(R.mipmap.ic_launcher));
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        TabLayout mTabLayout = (TabLayout) findViewById(R.id.tabLayout_top);
+//        mTabLayout.addTab(mTabLayout.newTab().setText("TabOne"));//给TabLayout添加Tab
+//        mTabLayout.addTab(mTabLayout.newTab().setText("TabTwo"));
+//        mTabLayout.addTab(mTabLayout.newTab().setText("TabThree"));
+//        mTabLayout.setupWithViewPager(mViewPager);//给TabLayout设置关联ViewPager，如果设置了ViewPager，那么ViewPagerAdapter中的getPageTitle()方法返回的就是Tab上的标题
+//
+//        ViewPager mViewPager = (ViewPager) findViewById(R.id.viewpager);
+//        MyViewPagerAdapter viewPagerAdapter = new MyViewPagerAdapter(getSupportFragmentManager());
+//        viewPagerAdapter.addFragment(FragmentOne.newInstance(), "TabOne");//添加Fragment
+//        viewPagerAdapter.addFragment(FragmentTwo.newInstance(), "TabTwo");
+//        viewPagerAdapter.addFragment(FragmentThree.newInstance(), "TabThree");
+//        mViewPager.setAdapter(viewPagerAdapter);//设置适配器
+
     }
 
     @Override
