68114d059bff6e6194d12248dc14f8086318cc7c
shenhuanet <shenhuanet@126.com>
2016-03-21 20:45:46 +0800

    2016-3-21 20：45

--- app/src/main/java/com/shenhua/reading/FragmentThree.java
+++ app/src/main/java/com/shenhua/reading/FragmentThree.java
@@ -6,7 +6,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class FragmentThree extends Fragment {
 
@@ -22,8 +21,6 @@ public class FragmentThree extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_fragment_three, container, false);
-        TextView mTextView = (TextView) view.findViewById(R.id.fragment_tv);
-        mTextView.setText("这是第三个Fragment");
         return view;
     }
 
