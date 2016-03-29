63df4722acb0a68190deedd05cea99edaa4b0574
shenhuanet <shenhuanet@126.com>
2016-03-29 18:13:06 +0800

    2016-3-29 18：12

---
+++ app/src/main/java/com/shenhua/reading/utils/MyStringUtils.java
@@ -0,0 +1,10 @@
+package com.shenhua.reading.utils;
+
+/**
+ * Created by shenhua on 2016/3/29.
+ */
+public class MyStringUtils {
+
+    public static final String[] tab_name_array = {"首页", "CSDN", "SegmentFault", "推酷", "红黑联盟", "开源中国", "开发者头条", "看源社区"};
+
+}
