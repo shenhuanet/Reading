113ff882d79c4a57f05ad8ec032a5a9bafa2156a
shenhuanet <shenhuanet@126.com>
2016-04-05 17:59:17 +0800

    add boommenu

--- app/src/main/java/com/shenhua/reading/activity/ActivityTest.java
+++ app/src/main/java/com/shenhua/reading/activity/ActivityTest.java
@@ -1,50 +1,53 @@
 package com.shenhua.reading.activity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import com.nightonke.boommenu.BoomMenuButton;
+import com.nightonke.boommenu.Types.DimType;
+import com.shenhua.reading.R;
 
 /**
  * Created by shenhua on 4/5/2016.
  */
 public class ActivityTest extends AppCompatActivity implements
-        BoomMenuButton.OnSubButtonClickListener,
-        BoomMenuButton.AnimatorListener {
+        BoomMenuButton.OnSubButtonClickListener {
 
+    private BoomMenuButton boomMenuButton;
+    private boolean isInit = false;
 
     @Override
-    public void toShow() {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
 
+        boomMenuButton = (BoomMenuButton) findViewById(R.id.boombtn);
+        boomMenuButton.setOnSubButtonClickListener(this);
+        boomMenuButton.setDimType(DimType.DIM_0);
     }
 
     @Override
-    public void showing(float fraction) {
-
-    }
-
-    @Override
-    public void showed() {
-
-    }
-
-    @Override
-    public void toHide() {
-
-    }
-
-    @Override
-    public void hiding(float fraction) {
-
-    }
-
-    @Override
-    public void hided() {
-
+    public void onBackPressed() {
+        if (boomMenuButton.isClosed())
+            super.onBackPressed();
+        else
+            boomMenuButton.dismiss();
     }
 
     @Override
     public void onClick(int buttonIndex) {
 
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        if (!isInit) {
+//            initBoom();
+//            initInfoBoom();
+        }
+        isInit = true;
     }
 }
